caf4e42b34f81983050a9cda9d0da07cf15cbba3
Teckeon <teckeon@outlook.com>
2018-07-03 16:09:32 -0500

    Backward exercise completed

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/udemy_MultipleLinearRegression.R
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/udemy_MultipleLinearRegression.R
@@ -39,3 +39,33 @@ summary(regressor)
 regressor =lm(formula = Profit ~ R.D.Spend + Marketing.Spend, data = dataset) # the dot (.) all independant variables
 
 summary(regressor)
+
+# Automatically select through looping
+
+backwardElimination <- function(x, sl) {
+  numVars = length(x)
+  for (i in c(1:numVars)){
+    regressor = lm(formula = Profit ~ ., data = x)
+    maxVar = max(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"])
+    if (maxVar > sl){
+      j = which(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"] == maxVar)
+      x = x[, -j]
+    }
+    numVars = numVars - 1
+  }
+  return(summary(regressor))
+}
+
+SL = 0.05
+dataset = dataset[, c(1,2,3,4,5)]
+backwardElimination(training_set, SL)
+
+
+
+
+
+
+
+
+
+
